8b70da0151d50ef040b32915b1be341ec8d521c8
Sandeep Singh <sandeepsinghwork@web.de>
2023-07-16 17:47:20 +0200

    add working and waiting time to RecipesOverview

--- components/Recipe/Recipe.js
+++ components/Recipe/Recipe.js
@@ -16,21 +16,58 @@ const RecipeImage = styled(Image)`
 
 const RecipeTitle = styled.p`
   text-align: left;
-  margin: ${(props) => (props.isTitle ? "5px 0" : "3px 0")};
-  font-weight: ${(props) => (props.isTitle ? "bold" : "normal")};
-  font-style: ${(props) => (props.isTitle ? "normal" : "italic")};
+  margin: ${(props) => (props.$isTitle ? "5px 0" : "3px 0")};
+  ${(props) => (props.$isTitle ? "font-weight:bold" : "font-style:italic")};
+
+  @media (max-width: 500px) {
+    font-size: 13px;
+  }
+`;
+
+const RecipeTime = styled.div`
+  position: absolute;
+  background-color: ${(props) =>
+    props.$isWorkingTime
+      ? "rgba(83, 134, 139, 0.85)"
+      : "rgba(127, 144, 172, 0.85)"};
+
+  color: white;
+
+  font-size: 14px;
+  left: 10px;
+  top: ${(props) => (props.$sWorkingTime ? "10px" : "35px")};
+  padding: 3px 10px;
+  border-radius: 9px;
+
+  @media (max-width: 500px) {
+    font-size: 11px;
+    left: 7px;
+    top: ${(props) => (props.$isWorkingTime ? "7px" : "28px")};
+    padding: 3px 5px;
+    border-radius: 7px;
+  }
 `;
 
 export default function Recipe({ recipe }) {
   return (
     <RecipeContainer>
-      <RecipeImage
-        src={require(`/assets/${recipe.picture}`).default}
-        alt={recipe.title}
-        width={300}
-        height={200}
-      />
-      <RecipeTitle isTitle>{recipe.title}</RecipeTitle>
+      <div style={{ position: "relative" }}>
+        <RecipeImage
+          src={require(`/assets/${recipe.picture}`).default}
+          alt={recipe.title}
+          width={300}
+          height={200}
+        />
+        <RecipeTime $isWorkingTime>
+          Arbeitszeit {recipe.workingTime} Min.
+        </RecipeTime>
+        {recipe.waitingTime > 0 && (
+          <RecipeTime>
+            <div>Wartezeit {recipe.waitingTime} Min.</div>
+          </RecipeTime>
+        )}
+      </div>
+      <RecipeTitle $isTitle>{recipe.title}</RecipeTitle>
       <RecipeTitle>{recipe.subtitle}</RecipeTitle>
     </RecipeContainer>
   );
